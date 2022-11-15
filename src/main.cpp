eaf64572a19a6406620857678332d4cfa2a3d54c
high_hopes_58 <modeller753@gmail.com>
2022-11-15 21:45:28 +0200

    Create ShaderProgram class (shader load class)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "Renderer/ShaderProgram.h"
+
 
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
@@ -92,7 +94,18 @@ int main(void)
 
 	glClearColor(0, 1, 0, 1);
 
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	std::string vertexShader(vertex_shader);
+	std::string fragmentShader(fragment_shader);
+
+	Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
+
+	if (!shaderProgram.isCompiled())
+	{
+		std::cerr << "Can't create shader program!" << std::endl;
+		return -1;
+	}
+
+	/*GLuint vs = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vs, 1, &vertex_shader, nullptr);
 	glCompileShader(vs);
 	
@@ -106,7 +119,7 @@ int main(void)
 	glLinkProgram(shader_program);
 
 	glDeleteShader(vs);
-	glDeleteShader(fs);
+	glDeleteShader(fs);*/
 
 	GLuint points_vbo = 0;
 	glGenBuffers(1, &points_vbo);
@@ -140,7 +153,8 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader_program);
+		shaderProgram.use();
+		/*glUseProgram(shader_program);*/
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
